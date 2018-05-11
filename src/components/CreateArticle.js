2d60797f621f57d87b547ec6cb9648241e2bc455
Tomislav Bermanec <tbermanec@gmail.com>
2018-05-11 19:50:33 +0200

    Add input Fields To CreateArticle Component

--- src/components/CreateArticle.js
+++ src/components/CreateArticle.js
@@ -1,5 +1,7 @@
 import React, { Component } from "react";
+import { Button, Form, FormGroup, Label, Input, FormText } from "reactstrap";
 import axios from "axios";
+
 import "./CreateArticle.css";
 
 class CreateArticle extends Component {
@@ -14,6 +16,32 @@ class CreateArticle extends Component {
     return (
       <div>
         <h1>Add New Article</h1>
+        <Form>
+          <FormGroup>
+            <Label for="title">Article Title</Label>
+            <Input
+              type="text"
+              name="title"
+              id="title"
+              placeholder="Article Title"
+            />
+          </FormGroup>
+          <FormGroup>
+            <Label for="author">Author</Label>
+            <Input
+              type="text"
+              name="author"
+              id="author"
+              placeholder="Author Name"
+            />
+          </FormGroup>
+          <FormGroup>
+            <Label for="url">URL</Label>
+            <Input type="url" name="url" id="url" placeholder="URL" />
+          </FormGroup>
+          <Button>Submit</Button>
+          <Button>Cancel</Button>
+        </Form>
       </div>
     );
   }
