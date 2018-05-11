6b502bfee0916259ec599db7cc9ef417f3daa2a0
Tomislav Bermanec <tbermanec@gmail.com>
2018-05-11 19:17:43 +0200

    Edit Article Schema

--- server/articleModel.js
+++ server/articleModel.js
@@ -3,6 +3,8 @@ const mongoose = require("mongoose");
 const ArticleSchema = new mongoose.Schema({
   title: String,
   author: String,
+  url: String,
+  date: { type: Date, default: Date.now },
   text: String,
   votes: { type: Number, default: 0 }
 });
