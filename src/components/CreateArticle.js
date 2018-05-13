26ae2fb40cab73997dbb4b5c82a5c42058473fa5
Tomislav Bermanec <tbermanec@gmail.com>
2018-05-14 01:10:44 +0200

    Edit CreateArticle Form

--- src/components/CreateArticle.js
+++ src/components/CreateArticle.js
@@ -1,5 +1,15 @@
 import React, { Component } from "react";
-import { Button, Form, FormGroup, Label, Input, FormText } from "reactstrap";
+import {
+  Button,
+  Form,
+  FormGroup,
+  Label,
+  Input,
+  FormText,
+  Container,
+  Row,
+  Col
+} from "reactstrap";
 import axios from "axios";
 
 import "./CreateArticle.css";
@@ -9,40 +19,86 @@ class CreateArticle extends Component {
     super(props);
 
     this.state = {
-      characters: []
+      title: "",
+      author: "Unknown",
+      url: "http://www.index.hr"
     };
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
   }
+
+  handleChange(event) {
+    console.log("key pressed: ", event.target.value);
+    this.setState({ title: event.target.value });
+  }
+
+  handleSubmit(event) {
+    console.log("Paket je spreman na slanje: ", this.state);
+    axios
+      .post("http://localhost:3001/articles", this.state)
+      .then(function(response) {
+        console.log(response);
+      })
+      .catch(function(error) {
+        console.log(error);
+      });
+    // axios.post('/user', {
+    //   firstName: 'Fred',
+    //   lastName: 'Flintstone'
+    // })
+    // .then(function (response) {
+    //   console.log(response);
+    // })
+    // .catch(function (error) {
+    //   console.log(error);
+    // });
+    event.preventDefault();
+  }
+
   render() {
     return (
-      <div>
-        <h1>Add New Article</h1>
-        <Form>
-          <FormGroup>
-            <Label for="title">Article Title</Label>
-            <Input
-              type="text"
-              name="title"
-              id="title"
-              placeholder="Article Title"
-            />
-          </FormGroup>
-          <FormGroup>
-            <Label for="author">Author</Label>
-            <Input
-              type="text"
-              name="author"
-              id="author"
-              placeholder="Author Name"
-            />
-          </FormGroup>
-          <FormGroup>
-            <Label for="url">URL</Label>
-            <Input type="url" name="url" id="url" placeholder="URL" />
-          </FormGroup>
-          <Button>Submit</Button>
-          <Button>Cancel</Button>
-        </Form>
-      </div>
+      <Container>
+        <Row>
+          <Col sm="12" md={{ size: 6, offset: 3 }}>
+            <h1>Add New Article</h1>
+            <Form onSubmit={this.handleSubmit}>
+              <FormGroup>
+                <Label for="title">Article Title</Label>
+                <Input
+                  type="text"
+                  name="title"
+                  id="title"
+                  value={this.state.title}
+                  onChange={this.handleChange}
+                />
+              </FormGroup>
+              <FormGroup>
+                <Label for="author">Author</Label>
+                <Input
+                  type="text"
+                  name="author"
+                  id="author"
+                  value={this.state.author}
+                  onChange={this.handleChange}
+                />
+              </FormGroup>
+              {/* <FormGroup>
+                <Label for="url">URL</Label>
+                <Input
+                  type="url"
+                  name="url"
+                  id="url"
+                  value={this.state.url}
+                  onChange={this.handleChange}
+                />
+              </FormGroup> */}
+              <Button onSubmit={this.handleSubmit}>Submit</Button>
+              <Button>Cancel</Button>
+            </Form>
+          </Col>
+        </Row>
+      </Container>
     );
   }
 }
