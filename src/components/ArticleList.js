2d52b8292782e094786319183b20a7e7e419e017
Tomislav Bermanec <tbermanec@gmail.com>
2018-05-14 22:21:46 +0200

    Add Responsiveness to ArticleList

--- src/components/ArticleList.js
+++ src/components/ArticleList.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 import axios from "axios";
-import { Button } from "reactstrap";
+import { Button, Container, Row, Col } from "reactstrap";
 import { Link } from "react-router-dom";
 
 import "./ArticleList.css";
@@ -29,23 +29,27 @@ class ArticleList extends Component {
 
   render() {
     return (
-      <div>
-        <h1>Articles</h1>
-        {this.state.articles.map(article => (
-          <div className="card">
-            <a href={article.url}>
-              <h1>{article.title}</h1>
-            </a>
-            <h3>Autor: {article.author}</h3>
-            <p>{article.text}</p>
-            <p>Votes: {article.votes}</p>
-            <p>Date: {article.date}</p>
-          </div>
-        ))}
-        <Link to="/articles/add">
-          <Button color="primary">Add New Article</Button>
-        </Link>
-      </div>
+      <Container>
+        <Row>
+          <Col sm="12" md={{ size: 6, offset: 3 }}>
+            <h1>Articles</h1>
+            {this.state.articles.map(article => (
+              <div className="card">
+                <a href={article.url}>
+                  <h1>{article.title}</h1>
+                </a>
+                <h3>Autor: {article.author}</h3>
+                <p>{article.text}</p>
+                <p>Votes: {article.votes}</p>
+                <p>Date: {article.date}</p>
+              </div>
+            ))}
+            <Link to="/add">
+              <Button color="primary">Add New Article</Button>
+            </Link>
+          </Col>
+        </Row>
+      </Container>
     );
   }
 }
