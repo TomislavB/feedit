6920deb14a272aa3d62f3f8934a2018799c16f02
Tomislav Bermanec <tbermanec@gmail.com>
2018-05-20 20:08:53 +0200

    Add Routes

--- src/router.js
+++ src/router.js
@@ -4,10 +4,11 @@ import { Switch, Route } from "react-router-dom";
 import App from "./App";
 import ArticleList from "./components/ArticleList";
 import CreateArticle from "./components/CreateArticle";
+import Home from "./components/Home";
 
 export default (
   <Switch>
-    {/* <Route component={App} path="/" exact /> */}
+    <Route component={Home} path="/" exact />
     <Route component={ArticleList} path="/articles" />
     <Route component={CreateArticle} path="/add" />
   </Switch>
