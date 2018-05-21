b6cd4b50541dbf13163d3683a2f751a7079514a6
Tomislav Bermanec <tbermanec@gmail.com>
2018-05-21 21:05:59 +0200

    Edit Delete method in Article List COmponent

--- src/components/ArticleList.js
+++ src/components/ArticleList.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import axios from "axios";
+// import toast from "toast";
 import { Button, Container, Row, Col } from "reactstrap";
 import { Link } from "react-router-dom";
 import Article from "./Article";
@@ -14,6 +15,17 @@ class ArticleList extends Component {
       articles: []
     };
   }
+
+  handleDelete = _id => {
+    axios.delete("http://localhost:3000/${_id}").then(function(response) {
+      const { articles } = this.state;
+      const updatedList = articles.filter(
+        ({ _id: articleId }) => _id !== articleId
+      );
+      this.setState({ articles: updatedList });
+    });
+    // .catch(() => toast.error("Failed to delete article."));
+  };
 
   componentDidMount() {
     axios
